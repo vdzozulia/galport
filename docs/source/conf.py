14ce325e693c43e8e5c089f47dbab1fb94d1fdca
vdzozulia <vdzozulia.astro@gmail.com>
2026-02-24 17:08:14 +0300

    change docs 7

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -1,8 +1,8 @@
 # docs/source/conf.py
-import os
-import sys
+# import os
+# import sys
 
-sys.path.insert(0, os.path.abspath('../../src'))
+# sys.path.insert(0, os.path.abspath('../../src'))
 
 # -- Project information
 
@@ -15,11 +15,8 @@ version = '0.1.0'
 # -- General configuration
 
 extensions = [
-    'sphinx.ext.duration',
-    'sphinx.ext.doctest',
     'sphinx.ext.autodoc',
     'sphinx.ext.autosummary',
-    'sphinx.ext.intersphinx',
     'sphinx.ext.viewcode',
     'sphinx.ext.napoleon',
     'nbsphinx'
