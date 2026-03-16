d748d690bec1e3a183dfc3ab977a21456a13afdd
vdzozulia <vdzozulia.astro@gmail.com>
2026-03-16 15:09:35 +0300

    change docs 15

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -12,27 +12,31 @@ author = 'Zozulia Viktor'
 release = '0.1'
 version = '0.1.0'
 
+# -- General configuration
 
-# -- General configuration ---------------------------------------------------
-# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+xtensions = [
+    'sphinx.ext.duration',
+    'sphinx.ext.doctest',
+    'sphinx.ext.autodoc',
+    'sphinx.ext.autosummary',
+    'sphinx.ext.intersphinx',
+    'sphinx.ext.viewcode',
+    'sphinx.ext.napoleon',           # Для NumPy-style docstrings
+    'nbsphinx',                        # Для Jupyter notebooks
+    'sphinx_copybutton',               # Для копирования кода
+]
 
-extensions = ["sphinx.ext.autodoc", "sphinx.ext.napoleon", "sphinx.ext.viewcode", "nbsphinx"]
-#nbsphinx_prompt_width = 0 # no prompts in nbsphinx
+intersphinx_mapping = {
+    'python': ('https://docs.python.org/3/', None),
+    'sphinx': ('https://www.sphinx-doc.org/en/master/', None),
+}
+intersphinx_disabled_domains = ['std']
 
 templates_path = ['_templates']
-exclude_patterns = ['.ipynb_checkpoints/*']
 
-
-
-# -- Options for HTML output -------------------------------------------------
-# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
-
-#html_theme = 'alabaster'
+# -- Options for HTML output
 
 html_theme = 'sphinx_rtd_theme'
-#html_theme = 'agogo'
-html_static_path = ['_static']
 
-#body_max_width = 1200
-master_doc = 'index'
-
+# -- Options for EPUB output
+epub_show_urls = 'footnote'
